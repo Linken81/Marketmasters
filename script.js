21b9b801f208790270e5e6e43345488a2fd04e6d
Linken81 <linus_bergsten@hotmail.com>
2025-10-27 16:39:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1080,7 +1080,6 @@ if (researchBtn) {
     });
   };
 }
-}
 // ------------------ Modals ------------------
 function openModal(id) { const m = document.getElementById(id); if (m) m.setAttribute('aria-hidden', 'false'); }
 function closeModal(id) { const m = document.getElementById(id); if (m) m.setAttribute('aria-hidden', 'true'); }
